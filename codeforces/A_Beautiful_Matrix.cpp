b6cb4e88a0ba7f683c79b881cca3c32ba696c54a
Cv Kamboj <98317391+cvkamboj@users.noreply.github.com>
2022-10-08 13:24:54 +0530

    Add files via upload

---
+++ codeforces/A_Beautiful_Matrix.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int x = 0;
+    for (int i = 1; i <= 5; ++i)
+    {
+        for (int j = 1; j <= 5; ++j)
+        {
+            cin >> x;
+            if (x)
+            {
+                cout << abs(i - 3) + abs(j - 3) << endl;
+            }
+        }
+    }
+    return 0;
+}
