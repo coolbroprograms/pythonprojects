ba190eb5a3eb6a7ce4b11911d71d6355b4dee94c
Sourav Saini <83644254+Sourav279@users.noreply.github.com>
2022-10-27 12:11:07 +0530

    Create Docstring.py

---
+++ Docstring.py
@@ -0,0 +1,19 @@
+#  DOCSTRING
+
+def sum(num1,num2):
+
+  '''
+
+  this function gives sum of two parameters
+
+  '''
+
+  print(num1+num2)
+
+sum(10,5)
+
+help(sum)
+
+print('\n')
+
+print(sum.__doc__)
